30693d6e89d5d89668fcbff1a6a083721705e6d1
noko <nishimura.r.019@ms.saitama-u.ac.jp>
2025-03-16 04:58:44 +0900

    29

--- discordbot.py
+++ discordbot.py
@@ -33,6 +33,10 @@ async def add(ctx, left: int, right: int):
 async def etatoto(ctx, flag: str):
     """send url"""
     if(flag==config.SECRET):
+        ch_name = "コマンド"
+        for channel in bot.get_all_channels():
+		    if channel.name == ch_name:
+			    await channel.send("起動しました")
         await ctx.send("ok")
 
 @bot.command()
